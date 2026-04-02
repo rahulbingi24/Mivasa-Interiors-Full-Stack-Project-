ea319258eefe76aeac417d40fb8b45c13fca161d
rahul bingi <rahulbingi248@gmail.com>
2026-04-02 16:18:53 +0530

    UI Development completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
-import Hero from './components/Hero/Hero'
-import About from './components/About/About'
-import Gallery from './components/Gallery/Gallery'
 import Footer from './components/Footer/Footer'
-import Projects from './Components/Projects/Projects'
 import AppRoutes from './routes/AppRoutes'
 
 const App = () => {
